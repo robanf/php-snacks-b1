da8c36948346a873a0e78b035720ec6e862914c1
Roberto <robertoanfuso98@gmail.com>
2021-07-26 14:57:06 +0200

    fine snack 1

--- parte1.php
+++ parte1.php
@@ -1,3 +1,30 @@
+<?php
+
+    $partite=[
+            [
+                'casa'=>'Milano',
+                'punteggioCasa'=>rand(50,130),
+                'avversari'=>'Cefalù',
+                'punteggioavv'=>rand(50,130),
+            ],
+            [
+                'casa'=>'Pizzocalabro',
+                'punteggioCasa'=>rand(50,130),
+                'avversari'=>'Carrapipi',
+                'punteggioavv'=>rand(50,130),
+            ],
+            [
+                'casa'=>'Donnalucata',
+                'punteggioCasa'=>rand(50,130),
+                'avversari'=>'Sampieri',
+                'punteggioavv'=>rand(50,130),
+            ],
+    ];
+
+?>
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,6 +34,16 @@
     <title>Document</title>
 </head>
 <body>
-    
+        
+        <ul>
+        <?php for($i=0;$i<count($partite);$i++){?>
+            <li>
+
+                 <?php   echo $partite[$i]['casa'] . "-" .  $partite[$i]['avversari'] . ' | ' . $partite[$i]['punteggioCasa'] . '-' . $partite[$i]['punteggioavv']; ?>
+                
+
+            </li>
+            <?php } ?>
+        </ul>
 </body>
 </html>
